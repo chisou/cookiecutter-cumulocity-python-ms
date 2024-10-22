142d8aaa683bee551655198a85afb24d1ed03d2d
Christoph Souris <christoph.souris@softwareag.com>
2024-10-22 17:23:13 +0200

    Initial revision.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,25 @@
+
+import os
+import subprocess
+
+if __name__ == '__main__':
+
+    if '{{ cookiecutter.license }}' == 'No license':
+        os.remove('LICENSE')
+        
+    src = 'src/main'
+    if '{{ cookiecutter.entrypoint }}' == 'Multi-Tenant Microservice':
+        os.rename(f'{src}/multi_tenant.py', f'{src}/main.py')
+    elif '{{ cookiecutter.entrypoint }}' == 'Single-Tenant Microservice':
+        os.rename(f'{src}/single_tenant.py', f'{src}/main.py')
+
+
+    if {{ cookiecutter.create_venv }}:
+        dir = '.venv'
+        print(f"Creating venv in directory '{dir}' ...")
+        subprocess.run(['python3', '-m', 'venv', dir], check=True)
+        print(f"Installing development dependencies ...")
+        subprocess.run([f'{dir}/bin/python', '-m', 'pip', 'install', '-r', 'requirements.txt'], check=True)
+        subprocess.run([f'{dir}/bin/python', '-m', 'pip', 'install', '-r', 'requirements-dev.txt'], check=True)
+
+    print("\nProject successfully created: {{ cookiecutter.project_slug }}")
