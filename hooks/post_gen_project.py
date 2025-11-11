8ecff46bc5beac22f641fce35e9c8903d23011ee
Christoph Souris <christoph.souris@cumulocity.com>
2025-11-11 10:47:45 +0100

    Better pip init.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         print(f"Creating venv in directory '{dir}' ...")
         subprocess.run(['python3', '-m', 'venv', dir], check=True)
         print(f"Installing development dependencies ...")
-        subprocess.run([f'{dir}/bin/python', '-m', 'pip', 'install', '-r', 'requirements.txt'], check=True)
-        subprocess.run([f'{dir}/bin/python', '-m', 'pip', 'install', '-r', 'requirements-dev.txt'], check=True)
+        subprocess.run([f'{dir}/bin/pip', 'install', '-r', 'requirements.txt'], check=True)
+        subprocess.run([f'{dir}/bin/pip', 'install', '-r', 'requirements-dev.txt'], check=True)
 
     print("\nProject successfully created: {{ cookiecutter.project_slug }}")
